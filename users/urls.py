4aa2eb8656e731ffca0e82abb4ed86f165a040db
L0L1P0P1 <behradbadeli@gmail.com>
2025-09-11 01:30:39 +0330

    good shtuff

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
+from django.conf import settings
 
 urlpatterns = [
     path('', views.hub, name='hub'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page=settings.LOGOUT_REDIRECT_URL), name='logout'),
     path('signup/', views.signup, name='signup'),
     path('signup/student/', views.student_signup, name='student_signup'),
     path('signup/instructor/', views.instructor_signup, name='instructor_signup'),
