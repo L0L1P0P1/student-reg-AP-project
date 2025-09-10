d07b478774b75f9045256e3a04b6c887b8da245f
L0L1P0P1 <behradbadeli@gmail.com>
2025-09-10 10:57:02 +0330

    added some views, changed some models

--- instructors/views.py
+++ instructors/views.py
@@ -1,15 +1,26 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .users import Instructor, Student, User
+from .courses import Unit, Course, CourseStudentStatus 
 
 @login_required(login_url="/login/")
-def active_classes(request):
+def instructor_classes(request):
     user = request.user
 
     if user.role != User.role.INSTRUCTOR:
         return HttpResponse('401 Unauthorized: You are likely not permitted to see this page with your current authorization.', status=401) # pyright: ignore 
-    else:
-        pass
+    
+    current_classes = Course.objects.filter(semester__active=True, instructor=user) # I'll add a logic in the template to filter by active/not active
+
+    return HttpResponse(current_classes[0], status=200)
+
+@login_required(login_url='/login/')
+def class_management(request, pk):
+    course = get_object_or_404(Course, pk=pk)
+    students = CourseStudentStatus.objects.filter(course=course).select_related("student")
+    
+    # for now
+    pass 
 
 
